6dbdb754b79b8139bd8979c8a9506bfb6175622c
Brigid Michelle Syondie <brigidmichelle02@gmail.com>
2025-04-23 08:58:06 +0300

    Updated assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,45 @@
-import React from "react";
-import Header from "./Header";
-import PlantPage from "./PlantPage";
+import React, { useEffect, useState } from "react";
+import PlantList from "./PlantList";
+import NewPlantForm from "./NewPlantForm";
+import Search from "./Search";
+import "./App.css";
 
 function App() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then(setPlants);
+  }, []);
+
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
+  function handleSearchChange(term) {
+    setSearchTerm(term);
+  }
+
+  function handleToggleSoldOut(id) {
+    setPlants((plants) =>
+      plants.map((plant) =>
+        plant.id === id ? { ...plant, soldOut: !plant.soldOut } : plant
+      )
+    );
+  }
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="app">
-      <Header />
-      <PlantPage />
+      <h1>Plantsy</h1>
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search searchTerm={searchTerm} onSearchChange={handleSearchChange} />
+      <PlantList plants={filteredPlants} onToggleSoldOut={handleToggleSoldOut} />
     </div>
   );
 }
--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,23 @@
-import React from "react";
+// PlantCard.js
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ plant, onSoldOut }) {
+  const { id, name, image, price } = plant;
+  const [isSoldOut, setIsSoldOut] = useState(false);
+
+  function handleSoldOutClick() {
+    setIsSoldOut(!isSoldOut);
+    onSoldOut(id);
+  }
+
   return (
-    <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+    <li className="card">
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>{isSoldOut ? "SOLD OUT" : `$${price}`}</p>
+      <button onClick={handleSoldOutClick}>
+        {isSoldOut ? "In Stock" : "Sold Out"}
+      </button>
     </li>
   );
 }
